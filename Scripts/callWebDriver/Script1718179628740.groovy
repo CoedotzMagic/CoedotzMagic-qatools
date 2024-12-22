73c543c96fc6a9a5294ca6a9549626f9669f0448
poncoe <arief.wardhana@itasoft.co.id>
2024-12-22 21:29:22 +0700

    checkpoint

--- Scripts/callWebDriver/Script1718179628740.groovy
+++ Scripts/callWebDriver/Script1718179628740.groovy
@@ -14,9 +14,14 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
 import com.coedotzmagic.qatools.util.*
 
+def util = new Util()
 
+util.startScreenRecording()
+
+util.openBrowser("coedotzmagic.com")
+
+util.stopScreenRecording()
