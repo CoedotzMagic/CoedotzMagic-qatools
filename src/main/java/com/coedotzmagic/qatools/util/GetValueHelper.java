9a6ef2e653667f74a4520bdcfe0eff12d226b218
poncoe <arief.wardhana@itasoft.co.id>
2025-02-13 10:47:05 +0700

    create verify and get value

---
+++ src/main/java/com/coedotzmagic/qatools/util/GetValueHelper.java
@@ -0,0 +1,73 @@
+package com.coedotzmagic.qatools.util;
+
+import com.coedotzmagic.qatools.failurehandling.TellMeWhy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/*
+ * write by Coedotz
+ * 22-01-2025
+ */
+
+public class GetValueHelper {
+
+    /**
+     * <b>getTextElement()</b>
+     * digunakan untuk mendapatkan teks pada element
+     *
+     * <br><br>
+     *
+     * @param xpath
+     *
+     * @since 1.1
+     */
+    public String getTextElement(String xpath) {
+        WebDriver driver = DriverHelper.GetWebDriver();
+        try {
+            assert driver != null;
+            WebElement textElement = driver.findElement(By.xpath(xpath));
+            return textElement.getText();
+        } catch (Exception e) {
+            new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.NOT_FOUND_ELEMENT);
+            return "";
+        }
+    }
+
+    /**
+     * <b>getValueElement()</b>
+     * digunakan untuk mendapatkan value pada element
+     *
+     * <br><br>
+     *
+     * @param xpath
+     *
+     * @since 1.1
+     */
+    public String getValueElement(String xpath) {
+        WebDriver driver = DriverHelper.GetWebDriver();
+        try {
+            assert driver != null;
+            WebElement valueElement = driver.findElement(By.xpath(xpath));
+            return valueElement.getDomProperty("value");
+        } catch (Exception e) {
+            new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.NOT_FOUND_ELEMENT);
+            return "";
+        }
+    }
+
+    /**
+     * <b>getTitlePage()</b>
+     * digunakan untuk mendapatkan title halaman page
+     *
+     * <br><br>
+     *
+     * @since 1.1
+     */
+    public String getTitlePage() {
+        WebDriver driver = DriverHelper.GetWebDriver();
+        assert driver != null;
+        return driver.getTitle();
+    }
+
+}
---
+++ src/main/java/com/coedotzmagic/qatools/util/VerifyValueHelper.java
@@ -0,0 +1,86 @@
+package com.coedotzmagic.qatools.util;
+
+import com.coedotzmagic.qatools.failurehandling.TellMeWhy;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+/*
+ * write by Coedotz
+ * 31-01-2025
+ */
+
+public class VerifyValueHelper {
+
+    /**
+     * <b>VerifyTitlePage()</b>
+     * digunakan untuk verifikasi title halaman page
+     *
+     * <br><br>
+     *
+     * @param expect
+     *
+     * @since 1.1
+     */
+    void VerifyTitlePage(String expect) {
+        WebDriver driver = DriverHelper.GetWebDriver();
+        assert driver != null;
+        if (expect != null && !expect.equalsIgnoreCase("") && !driver.getTitle().equalsIgnoreCase(expect)) {
+            new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.FAILED_TO_VERIFY + expect + ", Current value/text is: " +  driver.getCurrentUrl());
+        }
+    }
+
+    /**
+     * <b>VerifyValueElement()</b>
+     * digunakan untuk verifikasi value pada element
+     *
+     * <br><br>
+     *
+     * @param xpath
+     * @param expect
+     *
+     * @since 1.1
+     */
+    void VerifyValueElement(String xpath, String expect) {
+        WebDriver driver = DriverHelper.GetWebDriver();
+        String currentValue;
+        try {
+            assert driver != null;
+            WebElement valueElement = driver.findElement(By.xpath(xpath));
+            currentValue = valueElement.getDomProperty("value");
+            assert currentValue != null;
+            if (!currentValue.equalsIgnoreCase(expect)) {
+                new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.FAILED_TO_VERIFY + expect + ", Current value/text is: " +  currentValue);
+            }
+        } catch (Exception e) {
+            new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.NOT_FOUND_ELEMENT);
+        }
+    }
+
+    /**
+     * <b>VerifyTextElement()</b>
+     * digunakan untuk verifikasi text pada element
+     *
+     * <br><br>
+     *
+     * @param xpath
+     * @param expect
+     *
+     * @since 1.1
+     */
+    void VerifyTextElement(String xpath, String expect) {
+        WebDriver driver = DriverHelper.GetWebDriver();
+        String currentText;
+        try {
+            assert driver != null;
+            WebElement textElement = driver.findElement(By.xpath(xpath));
+            currentText = textElement.getText();
+            if (!currentText.equalsIgnoreCase(expect)) {
+                new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.FAILED_TO_VERIFY + expect + " Current value/text is: " +  currentText);
+            }
+        } catch (Exception e) {
+            new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.NOT_FOUND_ELEMENT);
+        }
+    }
+
+}
