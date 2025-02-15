1478e88fba2b526db946c1e127f089b202d3e77f
poncoe <arief.wardhana@itasoft.co.id>
2025-02-15 15:22:26 +0700

    create class for kill process (coedotz dev only)

---
+++ src/main/java/com/coedotzmagic/qatools/failurehandling/StartKillProcess.java
@@ -0,0 +1,14 @@
+package com.coedotzmagic.qatools.failurehandling;
+
+/*
+ * write by Coedotz
+ * 15-02-2025
+ *
+ * only for development coedotz only.
+ */
+
+public class StartKillProcess {
+    public static void main(String[] args) {
+        new KillProcessWebDriver();
+    }
+}
