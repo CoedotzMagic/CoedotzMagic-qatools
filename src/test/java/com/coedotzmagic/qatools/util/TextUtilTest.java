8de399ac9f65d69b809cca4aeda915e2762d97ed
poncoe <arief.wardhana@itasoft.co.id>
2025-02-18 10:19:55 +0700

    static call in testunit TextUtil

--- src/test/java/com/coedotzmagic/qatools/util/TextUtilTest.java
+++ src/test/java/com/coedotzmagic/qatools/util/TextUtilTest.java
@@ -6,13 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TextUtilTest {
 
-    TextUtil textUtil = new TextUtil();
-
     @org.junit.jupiter.api.Test
     void extractTextFromList() {
         String targetText1 = "50250205_122848263712_INTERNET";
         List<String> listToSearch1 = List.of("internet", "voice", "iptv");
-        String result1 = textUtil.extractTextFromList(targetText1, listToSearch1);
+        String result1 = TextUtil.extractTextFromList(targetText1, listToSearch1);
         assertEquals("internet", result1, "The method should return 'internet'");
     }
 
@@ -20,15 +18,15 @@ class TextUtilTest {
     void extractText() {
         String targetText1 = "COEDOTZ ITA 12 SOFT 4 SKYYLUVIE";
         String textIWant = "Coedotz";
-        String result = textUtil.extractText(targetText1, textIWant);
+        String result = TextUtil.extractText(targetText1, textIWant);
         assertEquals("COEDOTZ", result, "The method should return 'COEDOTZ'");
     }
 
     @org.junit.jupiter.api.Test
     void copyToClipboard() {
         String targetText1 = "coedotzmagic";
-        textUtil.copyToClipboard(targetText1);
-        String result = textUtil.getClipboardContent();
+        TextUtil.copyToClipboard(targetText1);
+        String result = TextUtil.getClipboardContent();
         assertEquals("coedotzmagic", result, "The method should return 'coedotzmagic'");
     }
 }
