e1ee686e361868818e626cda6b18c42cb27ecb68
poncoe <arief.wardhana@itasoft.co.id>
2025-03-05 14:48:23 +0700

    create getCurrentXpathElement

--- src/main/java/com/coedotzmagic/qatools/util/ElementHelper.java
+++ src/main/java/com/coedotzmagic/qatools/util/ElementHelper.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /*
  * write by Coedotz
@@ -16,6 +18,7 @@ import java.time.Duration;
  */
 
 public class ElementHelper {
+    public static WebElement currentElement;
 
     /**
      * <b>getIdElement()</b>
@@ -37,7 +40,7 @@ public class ElementHelper {
             String dynamicId = (type.equalsIgnoreCase("id")) ? bodyElement.getDomAttribute("id") :
                     (type.equalsIgnoreCase("class")) ? bodyElement.getDomAttribute("class") :
                             (type.equalsIgnoreCase("name")) ? bodyElement.getDomAttribute("name") : null;
-
+            currentElement = bodyElement;
             assert dynamicId != null;
             if (!dynamicId.equalsIgnoreCase("")) {
                 return dynamicId;
@@ -88,6 +91,31 @@ public class ElementHelper {
      */
     public static String getNameElement(String xpathElement) {
         return getElement("name", xpathElement);
+    }
+
+    /**
+     * <b>getCurrentXpathElement()</b>
+     * used to get current xpath element
+     *
+     * <br><br>
+     *
+     * @since 1.3
+     */
+    public static String getCurrentXpathElement() {
+        if (currentElement != null) {
+            String elementString = currentElement.toString();
+
+            String regex = "xpath:\\s*(/[^\\]]+\\[[^\\]]+\\]?[^\\]]*)";
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(elementString);
+
+            if (matcher.find()) {
+                return matcher.group(1);
+            } else {
+                return "not found current xpath element.";
+            }
+        }
+        return "not found current xpath element.";
     }
 
 }
