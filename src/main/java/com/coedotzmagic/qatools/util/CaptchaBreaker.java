286d2cfd02fc339756a921d9cbd4c27fc6ebb273
poncoe <arief.wardhana@itasoft.co.id>
2025-03-13 15:49:48 +0700

    reflactoring rename captha to Captcha

--- src/main/java/com/coedotzmagic/qatools/util/CaptchaBreaker.java
+++ src/main/java/com/coedotzmagic/qatools/util/CaptchaBreaker.java
@@ -24,7 +24,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class CaptchaBreaker {
 
     /**
-     * <b>CapthaManual()</b>
+     * <b>CaptchaManual()</b>
      * used to handle captcha in manual
      *
      * <br><br>
@@ -33,7 +33,7 @@ public class CaptchaBreaker {
      *
      * @since 1.1
      */
-    public static void CapthaManual(int timeout) {
+    public static void CaptchaManual(int timeout) {
         try {
             Thread.sleep(timeout * 1000);
         } catch (Exception e) {
@@ -42,7 +42,7 @@ public class CaptchaBreaker {
     }
 
     /**
-     * <b>CapthaAutomation()</b>
+     * <b>CaptchaAutomation()</b>
      * used to handle captcha in self action / automatic
      *
      * <br><br>
@@ -52,54 +52,58 @@ public class CaptchaBreaker {
      *
      * @since 1.1
      */
-    public static void CapthaAutomation(String xpathCaptchaElement, String idElementInput) {
+    public static void CaptchaAutomation(String xpathCaptchaElement, String idElementInput) {
         WebDriver driver = DriverHelper.GetWebDriver();
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(FailureHandlingHelper.GetTimeoutWait()));
-        assert driver != null;
-        WebElement captchaImage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpathCaptchaElement)));
-        ElementHelper.currentElement = captchaImage;
-
-        File tessDataPath = new File("tessdata");
-        String tessDataPathString = tessDataPath.getAbsolutePath();
-
-        if (!tessDataPath.exists() && !tessDataPath.isDirectory()) {
-            new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.NOT_FOUND_OCR_TESSDATA);
-        }
-
-        File screenshot = captchaImage.getScreenshotAs(OutputType.FILE);
-        File folder = new File("Captcha");
-        if (!folder.exists()) {
-            folder.mkdirs();
-        }
-        File captchaImageFile = new File(folder, "captcha-" + DateTime.getDateTime() + ".png");
-        BufferedImage cleanedImage = null;
         try {
-            FileUtils.copyFile(screenshot, captchaImageFile);
+            assert driver != null;
+            WebElement captchaImage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpathCaptchaElement)));
+            ElementHelper.currentElement = captchaImage;
 
-            BufferedImage image = ImageIO.read(captchaImageFile);
-            cleanedImage = ImprovImgCaptcha.cleanImage(image);
-            ImageIO.write(cleanedImage, "png", captchaImageFile);
-        } catch (IOException e) {
-            new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.FAILED_TO_READWRITE_IMG + e.getMessage());
-        }
+            File tessDataPath = new File("tessdata");
+            String tessDataPathString = tessDataPath.getAbsolutePath();
 
-        Tesseract tesseract = new Tesseract();
-        tesseract.setDatapath(tessDataPathString);
+            if (!tessDataPath.exists() && !tessDataPath.isDirectory()) {
+                new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.NOT_FOUND_OCR_TESSDATA);
+            }
 
-        try {
-            String captchaText = tesseract.doOCR(cleanedImage);
-            captchaText = captchaText.replaceAll("[^a-zA-Z0-9]", "");
-            System.out.println("Captcha text: " + captchaText);
-            if (!captchaText.equalsIgnoreCase("")) {
-                TextUtil.InputTextField(idElementInput, captchaText);
-                CapthaManual(8);
-            } else {
-                TextUtil.InputTextField(idElementInput, TellMeWhy.UNABLE_EXTRACT_CAPTCHA);
-                new TellMeWhy("w", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.UNABLE_EXTRACT_CAPTCHA);
-                CapthaManual(8);
+            File screenshot = captchaImage.getScreenshotAs(OutputType.FILE);
+            File folder = new File("Captcha");
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+            File captchaImageFile = new File(folder, "captcha-" + DateTime.getDateTime() + ".png");
+            BufferedImage cleanedImage = null;
+            try {
+                FileUtils.copyFile(screenshot, captchaImageFile);
+
+                BufferedImage image = ImageIO.read(captchaImageFile);
+                cleanedImage = ImprovImgCaptcha.cleanImage(image);
+                ImageIO.write(cleanedImage, "png", captchaImageFile);
+            } catch (IOException e) {
+                new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.FAILED_TO_READWRITE_IMG + e.getMessage());
+            }
+
+            Tesseract tesseract = new Tesseract();
+            tesseract.setDatapath(tessDataPathString);
+
+            try {
+                String captchaText = tesseract.doOCR(cleanedImage);
+                captchaText = captchaText.replaceAll("[^a-zA-Z0-9]", "");
+                System.out.println("Captcha text: " + captchaText);
+                if (!captchaText.equalsIgnoreCase("")) {
+                    TextUtil.InputTextField(idElementInput, captchaText);
+                    CaptchaManual(8);
+                } else {
+                    TextUtil.InputTextField(idElementInput, TellMeWhy.UNABLE_EXTRACT_CAPTCHA);
+                    new TellMeWhy("w", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.UNABLE_EXTRACT_CAPTCHA);
+                    CaptchaManual(8);
+                }
+            } catch (Exception e) {
+                new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.ERROR_OCR + e.getMessage());
             }
         } catch (Exception e) {
-            new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.ERROR_OCR + e.getMessage());
+            new TellMeWhy("e", TellMeWhy.getTraceInfo(Thread.currentThread().getStackTrace()), TellMeWhy.UNABLE_TO + "Doing Captcha Automation: " + e.getMessage());
         }
     }
 }
