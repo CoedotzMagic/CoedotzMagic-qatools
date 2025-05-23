26635c3c7be5d1ad56b6b03a168febed8b0359eb
poncoe <arief.wardhana@itasoft.co.id>
2025-05-23 13:44:14 +0700

    add date time specific

--- src/main/java/com/coedotzmagic/qatools/util/DateTime.java
+++ src/main/java/com/coedotzmagic/qatools/util/DateTime.java
@@ -1,8 +1,11 @@
 package com.coedotzmagic.qatools.util;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Map;
+import java.util.HashMap;
 
 /*
  * write by Coedotz
@@ -10,6 +13,7 @@ import java.util.Date;
  */
 
 public class DateTime {
+    static Map<String, Integer> dateTime = getCurrentDateTimeParts();
 
     /**
      * <b>getDateTime()</b>
@@ -24,6 +28,85 @@ public class DateTime {
         String formattedDateTime = dateFormat.format(currentDate);
         formattedDateTime = formattedDateTime.replaceAll(":", ".");
         return formattedDateTime;
+    }
+
+    /**
+     * <b>getCurrentYear()</b>
+     * used to get current year
+     *
+     * @since 1.5.0
+     */
+    public static String getCurrentYear() {
+        return dateTime.get("year").toString();
+    }
+
+    /**
+     * <b>getCurrentMonth()</b>
+     * used to get current month
+     *
+     * @since 1.5.0
+     */
+    public static String getCurrentMonth() {
+        return dateTime.get("month").toString();
+    }
+
+    /**
+     * <b>getCurrentDay()</b>
+     * used to get current day
+     *
+     * @since 1.5.0
+     */
+    public static String getCurrentDay() {
+        return dateTime.get("day").toString();
+    }
+
+    /**
+     * <b>getCurrentHour()</b>
+     * used to get current hour
+     *
+     * @since 1.5.0
+     */
+    public static String getCurrentHour() {
+        return dateTime.get("hour").toString();
+    }
+
+    /**
+     * <b>getCurrentMinute()</b>
+     * used to get current minute
+     *
+     * @since 1.5.0
+     */
+    public static String getCurrentMinute() {
+        return dateTime.get("minute").toString();
+    }
+    /**
+     * <b>getCurrentSecond()</b>
+     * used to get current second
+     *
+     * @since 1.5.0
+     */
+    public static String getCurrentSecond() {
+        return dateTime.get("second").toString();
+    }
+
+    /**
+     * <b>getCurrentDateTimeParts()</b>
+     * used to get today's Date and Time separate & specific
+     *
+     * @since 1.5.0
+     */
+    private static Map<String, Integer> getCurrentDateTimeParts() {
+        LocalDateTime now = LocalDateTime.now();
+
+        Map<String, Integer> dateTimeParts = new HashMap<>();
+        dateTimeParts.put("year", now.getYear());
+        dateTimeParts.put("month", now.getMonthValue()); // 1 to 12
+        dateTimeParts.put("day", now.getDayOfMonth());
+        dateTimeParts.put("hour", now.getHour());
+        dateTimeParts.put("minute", now.getMinute());
+        dateTimeParts.put("second", now.getSecond());
+
+        return dateTimeParts;
     }
 
 }
